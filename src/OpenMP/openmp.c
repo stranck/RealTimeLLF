6194d5370ee1abb220aff4da6c5780bb16bbd268
Stranck <strdjn@gmail.com>
2022-03-26 17:17:29 +0100

    Openmp _should_ be ready to be tested

--- src/OpenMP/openmp.c
+++ src/OpenMP/openmp.c
@@ -58,9 +58,9 @@ void downsampleConvolve(Image3 *dest, Image3 *source, uint32_t *width, uint32_t 
 }
 
 void upsampleConvolve(Image3 *dest, Image3 *source, Kernel kernel){
-	uint32_t smallWidth = source->width, smallHeight = source->height;
-	uint32_t uppedW = smallWidth << 1;
-	uint32_t uppedH = smallHeight << 1;
+	const uint32_t smallWidth = source->width, smallHeight = source->height;
+	const uint32_t uppedW = smallWidth << 1;
+	const uint32_t uppedH = smallHeight << 1;
 	dest->width = uppedW;
 	dest->height = uppedH;
 	const uint8_t  rows = KERNEL_DIMENSION;
@@ -128,15 +128,116 @@ void laplacianPyramid(Pyramid laplacian, Pyramid tempGauss, uint8_t nLevels, Ker
 	imgcpy3(laplacian[nLevels], tempGauss[nLevels]);
 }
 
-void collapse(Image3 *dest, Pyramid laplacianPyr, uint8_t nLevels, Kernel filter){
+
+void downsampleConvolve_parallel(Image3 *dest, Image3 *source, uint32_t *width, uint32_t *height, Kernel filter, const uint8_t nThreads){
+	uint32_t originalW = *width, originalH = *height;
+	*width /= 2;
+	*height /= 2;
+	dest->width = *width;
+	dest->height = *height;
+	const int32_t startingX = originalW & 1;
+	const int32_t startingY = originalH & 1;
+	const int8_t  rows = KERNEL_DIMENSION;
+	const int8_t  cols = KERNEL_DIMENSION;
+	const int32_t  xstart = -1 * cols / 2;
+	const int32_t  ystart = -1 * rows / 2;
+	originalW -= startingX;
+	const uint32_t dim = (originalH - startingY * 2) * originalW; //not *2 on w because we need one extra pixel at the end of the line for the +=2 to work
+
+	#pragma omp parallel for num_threads(nThreads) schedule(static)
+	for(uint32_t idx = 0; idx < dim; idx += 2){
+		uint32_t i = (idx % originalW) + startingX, j = (idx / originalW) + startingY;
+
+		Pixel3 c = zero3f;
+		for (uint32_t y = 0; y < rows; y++) {
+			int32_t jy = j + (ystart + y) * 2 - startingY;
+			for (uint32_t x = 0; x < cols; x++) {
+				int32_t ix = i + (xstart + x) * 2 - startingX;
+
+				if (ix >= 0 && ix < originalW && jy >= 0 && jy < originalH) {
+					double kern_elem = filter[getKernelPosition(x, y)];
+					Pixel3 px = *getPixel3(source, ix, jy);
+
+					c.x += px.x * kern_elem;
+					c.y += px.y * kern_elem;
+					c.z += px.z * kern_elem;
+				} else {
+					
+					double kern_elem = filter[getKernelPosition(x, y)];
+					Pixel3 px = *getPixel3(source, i - startingX, j - startingY);
+
+					c.x += px.x * kern_elem;
+					c.y += px.y * kern_elem;
+					c.z += px.z * kern_elem;
+				}
+			}
+		}
+		setPixel3(dest, i / 2, j / 2, &c);
+	}
+}
+
+void upsampleConvolve_parallel(Image3 *dest, Image3 *source, Kernel kernel, const uint8_t nThreads){
+	const uint32_t smallWidth = source->width, smallHeight = source->height;
+	const uint32_t uppedW = smallWidth << 1;
+	const uint32_t uppedH = smallHeight << 1;
+	dest->width = uppedW;
+	dest->height = uppedH;
+	const uint8_t  rows = KERNEL_DIMENSION;
+	const uint8_t  cols = KERNEL_DIMENSION;
+	const int32_t  xstart = -1 * cols / 2;
+	const int32_t  ystart = -1 * rows / 2;
+	const uint32_t dim = uppedH * uppedW;
+
+	#pragma omp parallel for num_threads(nThreads) schedule(static)
+	for (uint32_t idx = 0; idx < dim; idx++) {
+		uint32_t i = idx % uppedW, j = idx / uppedW;
+
+		Pixel3 c = zero3f;
+		for (uint32_t y = 0; y < rows; y++) {
+			int32_t jy = (j + ystart + y) / 2;
+			for (uint32_t x = 0; x < cols; x++) {
+				int32_t ix = (i + xstart + x) / 2;
+				if (ix >= 0 && ix < smallWidth && jy >= 0 && jy < smallHeight) {
+					double kern_elem = kernel[getKernelPosition(x, y)];
+					Pixel3 px = *getPixel3(source, ix, jy);
+
+					c.x += px.x * kern_elem;
+					c.y += px.y * kern_elem;
+					c.z += px.z * kern_elem;
+				} else {
+					double kern_elem = kernel[getKernelPosition(x, y)];
+					Pixel3 px = *getPixel3(source, i / 2, j / 2);
+
+					c.x += px.x * kern_elem;
+					c.y += px.y * kern_elem;
+					c.z += px.z * kern_elem;
+				}
+			}
+		}
+		setPixel3(dest, i, j, &c);
+	}
+}
+
+void gaussianPyramid_parallel(Pyramid outPyr, Image3 *inImg, uint8_t nLevels, Kernel filter, const uint8_t nThreads){
+	imgcpy3_parallel(outPyr[0], inImg, nThreads);
+	uint32_t width = inImg->width, height = inImg->height;
+	//if(0 <= nLevels){ //So it don't need to copy two times the whole img
+		downsampleConvolve_parallel(outPyr[1], inImg, &width, &height, filter, nThreads);
+	//}
+	for(uint8_t i = 1; i < nLevels; i++)
+		downsampleConvolve_parallel(outPyr[i + 1], outPyr[i], &width, &height, filter, nThreads);
+}
+
+void collapse(Image3 *dest, Pyramid laplacianPyr, uint8_t nLevels, Kernel filter, const uint8_t nThreads){
 	Pixel3 *destPxs = dest->pixels;
 	for(int8_t lev = nLevels; lev > 1; lev--){ //Using dest as a temp buffer
 		Image3 *currentLevel = laplacianPyr[lev], *biggerLevel = laplacianPyr[lev - 1];
 		Pixel3 *biggerLevelPxs = biggerLevel->pixels;
 
-		upsampleConvolve(dest, currentLevel, filter);
+		upsampleConvolve_parallel(dest, currentLevel, filter, nThreads);
 		uint32_t sizeUpsampled = min(dest->width, biggerLevel->width) * min(dest->height, biggerLevel->height);
-		for(uint32_t px = 0; px < sizeUpsampled; px++)
+		#pragma omp parallel for num_threads(nThreads) schedule(static, 4)
+		for(uint32_t px = 0; px < sizeUpsampled; px++)	
 			biggerLevelPxs[px] = vec3Add(destPxs[px], biggerLevelPxs[px], Pixel3);
 		biggerLevel->width = dest->width;
 		biggerLevel->height = dest->height; //This could cause disalignment problem
@@ -144,13 +245,14 @@ void collapse(Image3 *dest, Pyramid laplacianPyr, uint8_t nLevels, Kernel filter
 	Image3 *currentLevel = laplacianPyr[1], *biggerLevel = laplacianPyr[0];
 	Pixel3 *biggerLevelPxs = biggerLevel->pixels;
 
-	upsampleConvolve(dest, currentLevel, filter);
+	upsampleConvolve_parallel(dest, currentLevel, filter, nThreads);
 	uint32_t sizeUpsampled = min(dest->width, biggerLevel->width) * min(dest->height, biggerLevel->height);
+	#pragma omp parallel for num_threads(nThreads) schedule(static, 4)
 	for(uint32_t px = 0; px < sizeUpsampled; px++)
 		destPxs[px] = vec3Add(destPxs[px], biggerLevelPxs[px], Pixel3);
 }
 
-void llf(Image3 *img, double sigma, double alpha, double beta, uint8_t nLevels, uint8_t nThreads){
+void llf(Image3 *img, double sigma, double alpha, double beta, uint8_t nLevels, const uint8_t nThreads){
 	uint32_t width = img->width, height = img->height;
 	nLevels = min(nLevels, 5);
 	nLevels = max(nLevels, 3);//int(ceil(std::abs(std::log2(min(width, height)) - 3))) + 2;
@@ -160,7 +262,7 @@ void llf(Image3 *img, double sigma, double alpha, double beta, uint8_t nLevels, 
 	Pyramid outputLaplacian = createPyramid(width, height, nLevels);
 
 	print("Creating first gauss pyramid");
-	gaussianPyramid(gaussPyramid, img, nLevels, filter);
+	gaussianPyramid_parallel(gaussPyramid, img, nLevels, filter, nThreads);
 	print("Entering main loop");
 	// Sadly, due to approxxximation in the downsample function, I can't use sum to calculate the pyramid dimension :(
 	//uint32_t t = (0b100 << (nLevels * 2));
@@ -179,7 +281,7 @@ void llf(Image3 *img, double sigma, double alpha, double beta, uint8_t nLevels, 
 	#pragma omp private(b)
 	CurrentLevelInfo cli;
 	#pragma omp private(cli)
-	#pragma omp parallel num_threads(nThreads)
+	#pragma omp parallel num_threads(nThreads) schedule(dynamic)
 	{
 		b = createBuffers(width, height, nLevels);
 		initLevelInfo(&cli, pyrDimensions, gaussPyramid);
@@ -188,7 +290,7 @@ void llf(Image3 *img, double sigma, double alpha, double beta, uint8_t nLevels, 
 	#pragma omp parallel for num_threads(nThreads)
 	for(uint32_t idx = 0; idx < end; idx++){
 		if(idx >= cli.nextLevelDimension) //Assuming ofc that idk only goes up for each thread
-			updateLevelInfo(&cli, pyrDimensions, gaussianPyramid);
+			updateLevelInfo(&cli, pyrDimensions, gaussPyramid);
 		uint32_t localIdx = idx - cli.prevLevelDimension;
 
 		uint8_t lev = cli.lev;
@@ -222,9 +324,9 @@ void llf(Image3 *img, double sigma, double alpha, double beta, uint8_t nLevels, 
 
 		setPixel3(outputLaplacian[lev], x, y, getPixel3(b.bufferLaplacianPyramid[lev], full_res_roi_x >> lev, full_res_roi_yShifted)); //idk why i had to shift those
 	}
-	imgcpy3(outputLaplacian[nLevels], gaussPyramid[nLevels]);
+	imgcpy3_parallel(outputLaplacian[nLevels], gaussPyramid[nLevels], nThreads);
 	print("Collapsing");
-	collapse(img, outputLaplacian, nLevels, filter);
+	collapse(img, outputLaplacian, nLevels, filter, nThreads);
 	
 	destroyPyramid(&gaussPyramid, nLevels);
 	destroyPyramid(&outputLaplacian, nLevels);
